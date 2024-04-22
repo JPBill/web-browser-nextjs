2584172a013ae29a26323cc0d7055b477cec95d6
Juan Pablo Bill <juampibill1995@gmail.com>
2024-04-22 17:54:28 -0300

    fetching data from google search API

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,3 +1,8 @@
-export default function ImageSearchPage() {
+export default async function ImageSearchPage({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&searchType=image`
+  );
+  const data = await response.json();
+  const results = data.items;
   return <div>ImageSearchPage</div>;
 }
